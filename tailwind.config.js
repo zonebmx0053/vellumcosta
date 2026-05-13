c3b6a3fd98aa47e74cbeaa6f64f86096549247e6
zonebmx0053 <zonebmx0053@gmail.com>
2026-05-13 20:27:55 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    darkMode: 'class',
+    content: ['./**/*.html'],
+    theme: {
+        extend: {
+            colors: {
+                brand: {
+                    bg: '#FAFAFA',
+                    gold: '#C5A059',
+                    dark: '#333333',
+                    darkmode: '#121212',
+                    darkcard: '#1E1E1E'
+                }
+            },
+            fontFamily: {
+                sans: ['Montserrat', 'sans-serif'],
+                serif: ['Playfair Display', 'serif']
+            }
+        }
+    },
+    plugins: []
+}
